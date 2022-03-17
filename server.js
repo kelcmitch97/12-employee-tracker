a7d7fcaf9acbd5f0e9117cfb31b73bf5fbb2e567
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-03-17 19:49:47 -0230

    add switch

--- server.js
+++ server.js
@@ -4,37 +4,62 @@ const mysql = require('mysql2');
 const consoleTable = require('console.table');
 
 // Connection to Mysql
-const connection = mysql.createConnection( {
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Lambeau#2021',
+    password: '',
     database: 'employee_information_db'
 },
-console.log('Connected to the employee_information database.')
+    console.log('Connected to the employee_information database.'),
+    startScreen()
 );
 
 function startScreen() {
 
     inqurier
-    .prompt( {
-        type: 'list',
-        name: 'start',
-        message: 'What would you like to do?',
-        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
-    })
-    .then( result => {
+        .prompt({
+            type: 'list',
+            name: 'start',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+        })
+        .then(function({start}) {
 
-        console.log("You selected: " + result.option);
+            switch (start) {
+                case "View All Departments":
+                    viewDepartments();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "View All Roles":
+                    viewRoles();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                default:
+                    quit();
+            }
+        });
+};
 
-        
+// Department functions
+function viewDepartments() {
+    console.log('here');
+};
 
 
-    })
-
-
-
-}
 
 
 // // Connection promise
