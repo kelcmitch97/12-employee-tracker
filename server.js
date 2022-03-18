01afdfe9a97edcc5cdbe8d2ab97dbc18076114cb
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-03-17 22:37:28 -0230

    add department

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '',
-    database: 'employee_information_db'
+    database: 'employee_information_db',
+    multipleStatements: true
 },
     console.log('Connected to the employee_information database.'),
     startScreen()
@@ -56,8 +57,85 @@ function startScreen() {
 
 // Department functions
 function viewDepartments() {
-    console.log('here');
+
+
+    const sql = `SELECT * FROM department`;
+
+    connection.promise().query(sql)
+        .then( ([rows]) => {
+            console.table('\n', rows);
+        })
+        .catch(console.log)
+        .then( () => startScreen());
 };
+
+function addDepartment() {
+
+    inqurier
+        .prompt({
+            type: 'input',
+            name: 'addDepartment',
+            message: 'What is the name of the department?'
+    })
+    .then(function ({answer}) {
+
+        const newDept = `INSERT INTO department(name) VALUES(?)`, [answer.addDepartment]
+
+        connection.promise().query(sql)
+        .then( ([rows]) => {
+            console.table('\n', rows);
+        })
+        .catch(console.log)
+        .then( () => startScreen());
+    })
+
+
+};
+
+function viewEmployees() {
+
+
+    const sql = `SELECT * FROM employee`;
+
+    connection.promise().query(sql)
+        .then( ([rows]) => {
+            console.table('\n', rows);
+        })
+        .catch(console.log)
+        .then( () => startScreen());
+};
+
+function addEmployee() {
+    console.log('Add employee:');
+};
+
+function updateEmployeeRole() {
+    console.log('Update employee role:');
+};
+
+function viewRoles() {
+
+
+    const sql = `SELECT * FROM role;`;
+
+    connection.promise().query(sql)
+        .then( ([rows]) => {
+            console.table('\n', rows);
+        })
+        .catch(console.log)
+        .then( () => startScreen());
+};
+
+function addRole() {
+    console.log('Add role:');
+};
+
+function quit() {
+    
+    connection.end();
+};
+
+
 
 
 
